2bf619079ac2ab3b124f0ccdce4809ef671fa187
“Annie <a.sun3@share.epsb.ca>
2023-05-16 10:24:13 -0600

    test

--- bear.py
+++ bear.py
@@ -8,3 +8,4 @@ class Bear(MySprite):
         MySprite.__init__(self)
         self.__FILE_LOC = IMAGE_FILE  ## ENCAPSULATION (protecting and hiding data through an interface)
         self._SURFACE = pygame.image.load(self.__FILE_LOC).convert_alpha()
+        self.__HEALTH = 2

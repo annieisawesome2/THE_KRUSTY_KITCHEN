50d11757c28d6eac1afd6076f51ca86ffa5d38b1
“Annie <a.sun3@share.epsb.ca>
2023-06-06 11:18:28 -0600

    finishing touches

--- boxes.py
+++ boxes.py
@@ -17,6 +17,8 @@ class Box(MySprite):
         self.HEALTH = 0
 
     def setHealth(self):
+        """setting health for bubble
+        """
         self.HEALTH -= 1
     
     def setColor(self, TUPLE):
@@ -37,52 +39,6 @@ class Box(MySprite):
 
 
 
-if __name__ == "__main__":
-    pygame.init()
-    
-    WINDOW = Window("Box Test")
-    BALL = Box(50, 50)
-    BALL.setSpeed(10)
-    BALL.setColor((0, 0, 255))
-    BRICK = Box(100, 150)
-    BRICK.setPosition((300, 300))
-    BRICK2 = Box(100, 150)
-    BRICK2.setPosition((800, 200))
-    BALL.setPosition((10, 50))
-
-    while True:
-        # -- INPUTS -- #
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-        
-        PRESSED_KEYS = pygame.key.get_pressed()
-
-        # -- PROCESSING -- #
-        #BOX.marqueeX(WINDOW.getWidth())
-        BALL.bounceX(WINDOW.getWidth())
-        BALL.bounceY(WINDOW.getHeight())
-        BALL.checkBoundaries(WINDOW.getWidth(), WINDOW.getHeight())
-
-
-        if BALL.isSpriteColliding(BRICK.getPOS(), BRICK.getDimensions()):
-            BRICK.setColor((0, 255, 255))
-        else:
-            BRICK.setColor((255, 255, 255))
-
-        if BALL.isSpriteColliding(BRICK2.getPOS(), BRICK2.getDimensions()):
-            BRICK2.setColor((0, 255, 255))
-        else:
-            BRICK2.setColor((255, 255, 255))
-
-
-        # -- OUTPUTS -- #
-        WINDOW.clearScreen()
-        WINDOW.getSurface().blit(BALL.getSurface(), BALL.getPOS())
-        WINDOW.getSurface().blit(BRICK.getSurface(), BRICK.getPOS())
-        WINDOW.getSurface().blit(BRICK2.getSurface(), BRICK2.getPOS())
-        WINDOW.updateFrame()
 
 
  
